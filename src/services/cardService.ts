0eebc02b2c75817cba8c0c536d0e5a2418d66b52
unknown <haddadiamirhosein.d@gmail.com>
2025-05-10 00:35:53 +0400

    add pairId fiture

--- src/services/cardService.ts
+++ src/services/cardService.ts
@@ -2,14 +2,17 @@
 export interface Card {
     id : string;
     icon : string;
+    pairId : string;
 }
 
 
 const cardService  = (Items : Card[]) : Card[] => {
+
+    const pairId = crypto.randomUUID()
     
     const duplicatedCards = Items.flatMap((item) => [
-        {id : crypto.randomUUID() , icon : item.icon},
-        {id : crypto.randomUUID() , icon : item.icon},
+        {pairId , id : crypto.randomUUID() , icon : item.icon},
+        {pairId , id : crypto.randomUUID() , icon : item.icon},
     ])
     
     const finalCard = duplicatedCards.sort(() => Math.random() - 0.5)
