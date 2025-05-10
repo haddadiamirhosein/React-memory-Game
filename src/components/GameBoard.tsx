a9b71c7d0f579cc08eb050662ee9612631953fd8
unknown <haddadiamirhosein.d@gmail.com>
2025-05-10 21:39:29 +0400

    add Restart Butoom

--- src/components/GameBoard.tsx
+++ src/components/GameBoard.tsx
@@ -1,13 +1,22 @@
 import Grid from "@mui/material/Grid";
 import { cards } from "../data/cards";
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import CardGame from "./CardGame";
 import cardService, { type Card } from "../services/cardService";
+import { Box } from "@mui/material";
+import Button from "@mui/material/Button";
 
 function GameBoard() {
   const [matchedCards, SetmatchedCards] = useState<string[]>([]);
   const [selectedcards, SetselectedCards] = useState<string[]>([]);
   const [newCards, SetnewCards] = useState<Card[]>([]);
+
+  const handleRestart = () => {
+    const newCard = cardService(cards);
+    SetnewCards(newCard);
+    SetselectedCards([]);
+    SetmatchedCards([]);
+  };
 
   const handleCardClick = (id: string) => {
     if (
@@ -56,20 +65,42 @@ function GameBoard() {
   }, [selectedcards, newCards]);
 
   return (
-    <Grid container spacing="20px">
-      {newCards.map((card) => (
-        <Grid size={{ xs: 6, md: 3, sm: 4 }} key={card.id}>
-          <CardGame
-            onHandleClick={() => handleCardClick(card.id)}
-            icon={card.icon}
-            isFlipped={
-              selectedcards.includes(card.id) || matchedCards.includes(card.id)
-            }
-            isMatched={matchedCards.includes(card.id)}
-          />
-        </Grid>
-      ))}
-    </Grid>
+    <Box
+      sx={{
+        backgroundColor: "#f0f2f5",
+        minHeight: "100vh",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        padding: 2,
+      }}
+    >
+      <Button
+        variant="contained"
+        color="primary"
+        onClick={handleRestart}
+        sx={{ mb: 3 }}
+      >
+        Restart
+      </Button>
+
+      <Grid container spacing="20px" justifyContent="center">
+        {newCards.map((card) => (
+          <Grid size={{ xs: 6, md: 3, sm: 4 }} key={card.id}>
+            <CardGame
+              onHandleClick={() => handleCardClick(card.id)}
+              icon={card.icon}
+              isFlipped={
+                selectedcards.includes(card.id) ||
+                matchedCards.includes(card.id)
+              }
+              isMatched={matchedCards.includes(card.id)}
+            />
+          </Grid>
+        ))}
+      </Grid>
+    </Box>
   );
 }
 
